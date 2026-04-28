13a007b6b0aaa8da7214999bb814ea21849efd9c
Zachariah R. Roper <roper063@umn.edu>
2026-04-28 15:53:22 -0500

    Data import and cleaning

--- R/final.R
+++ R/final.R
@@ -1,10 +1,14 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-# Lib
+library(tidyverse)
 # Lib
 # Lib
 
 # Data Import and Cleaning
+import_data <- read_csv("../data/glassdoor_reviews.csv")
 
+cleaned_data <- import_data %>%
+  filter(!is.na(overall_rating)) %>% # Filter to ensure zero missing data in outcome
+  select(overall_rating, headline, pros, cons) # In an actual project I'd keep the other data, but sisnce it isnt needed here I only selected necisary variables to reduce processing time.
 
 # Analysis
