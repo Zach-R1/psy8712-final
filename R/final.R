cab0ade056fa63cabf4538abf26a0c5f88f4a75e
Zachariah R. Roper <roper063@umn.edu>
2026-05-05 22:37:47 -0500

    Edit emoji_transformer, added parallelized text processing, containerized corpus

--- R/final.R
+++ R/final.R
@@ -1,4 +1,5 @@
 # Script Settings and Resources
+set.seed(777)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(tm)
@@ -6,47 +7,95 @@ library(qdap)
 library(textstem)
 library(stringi)
 library(RWeka)
+library(parallel)
+library(doParallel)
 #library()
 # library() look at ollama stuff
 # Lib
+
+n_cores <- detectCores() - 1
 
 # Data Import and Cleaning
 import_data <- read_csv("../data/glassdoor_reviews.csv")
 
 cleaned_data <- import_data %>%
-  filter(!is.na(overall_rating)) %>% # Filter to ensure zero missing data in outcome
-  select(overall_rating, headline, pros, cons) # In an actual project I'd keep the other data, but sisnce it isnt needed here I only selected necisary variables to reduce processing time.
+  filter(!is.na(overall_rating)) %>% 
+  mutate(
+    doc_id = row_number()
+    ) %>%
+  unite(review, headline, pros, cons, sep = " ", na.rm = TRUE) %>% # Filter to ensure zero missing data in outcome
+  select(overall_rating, review, doc_id) # In an actual project I'd keep the other data, but since it isnt needed here I only selected necisary variables to reduce processing time.
+
+# This was a hard decision. Originally I planned to make three seperate corpora for headline, pros, and cons. Since the goal of the assignment is the best model in terms of prediction I chose not to. It'd help with interpretability, but couldn't find any info suggesting it would help prediction. So the additional effort and processing time didnt seem worth it This also deals with the issue later in the process where the transformation process would leave blanks in headline
+
 
 
 # Maybe change sections later for the below
-emoji_transformer <- function(x) { # Dataset contains significant enough emjoi use to want transformation of at least the more common or more meaningfully used emojis. Built function to replace emojis unicode with the emoji names
+# Dataset contains a decent enough emoji use that I didnt think it'd hurt to build a seperte transformation function. Also I wanted to see if I could get it to work.
+
+emoji_transformer <- function(x) {
   x <- stri_replace_all_regex(x, "[\U0001F3FB-\U0001F3FF]", "")
   
   patterns <- c(
-    "\U0001F44D" = "thumbs up ", # For each transform unicode to name and add space to the to avoid multiple words smooshed together for mutiple emoji use
-    "\U0001F44E" = "thumbs down ",
-    "\U0001F44C" = "okay ",
-    "\U0001F525" = "fire",
-    
-    "\U0001F60A" = "smile ",
-    "\U0001F60C" = "smile ",
-    "\U0001F601" = "smile ",
-    "\U0001F642" = "smile ",
-    
-    "\U0001F61E" = "frown ",
-    "\U0001F615" = "frown ",
-    "\U0001F641" = "frown "
+    "\U0001F44D", "\U0001F44E", "\U0001F44C", "\U0001F525",
+    "\U0001F60A", "\U0001F60C", "\U0001F601", "\U0001F642",
+    "\U0001F61E", "\U0001F615", "\U0001F641"
   )
   
-  for (p in names(patterns)) {
-    x <- stri_replace_all_fixed(x, p, patterns[[p]])
-  }
+  replacements <- c(
+    "thumbs up ", "thumbs down ", "okay ", "fire ",
+    "smile ", "smile ", "smile ", "smile ",
+    "frown ", "frown ", "frown "
+  )
   
-  return(x) 
+  x <- stri_replace_all_fixed(x, patterns, replacements, vectorize_all = FALSE)
+  
+  return(x)
 }
 
+sw_pattern <- paste0("\\b(", paste(stopwords("en"), collapse = "|"), ")\\b")
 # Test <- cleaned_data %>%
-#   mutate(headline = emoji_transformer(cleaned_data$headline)) Tested functionality
+#   mutate(headline = emoji_transformer(cleaned_data$review)) # Tested functionality
+
+
+cl <- makeCluster(n_cores)
+clusterEvalQ(cl, {
+  library(stringr)
+  library(qdap)
+  library(stringi)
+  library(textstem)
+})
+clusterExport(cl, c("emoji_transformer", "sw_pattern"))
+
+chunks <- split(cleaned_data$review, cut(seq_along(cleaned_data$review), n_cores))
+
+results <- parLapply(cl, chunks, function(x) {
+  x %>%
+    emoji_transformer() %>%
+    replace_contraction() %>%
+    replace_abbreviation() %>%
+    str_to_lower() %>%
+    str_remove_all("[[:punct:]]") %>%
+    str_remove_all("[[:digit:]]") %>%
+    lemmatize_strings() %>%
+    str_remove_all(regex(sw_pattern, ignore_case = TRUE)) %>%
+    str_squish()
+})
+
+stopCluster(cl)
+
+processed_data <- cleaned_data %>%
+  mutate(review_processed = unlist(results))
+
+
+
+# Containerize
+review_corpus_original <- VCorpus(VectorSource(processed_data$review_processed))
+
+
+
+
+# Plan: dtm, dtmslim, see if you should add additional stop words, model with tokens, and topic, then do embeddings POST, do the rest of the modeling, answer questions.
 
 
 
@@ -54,46 +103,30 @@ emoji_transformer <- function(x) { # Dataset contains significant enough emjoi u
 
 
 
-headline_corpus_original <- VCorpus(VectorSource(cleaned_data$headline))
 
-pros_corpus_original <- VCorpus(VectorSource(cleaned_data$pros))
 
-cons_corpus_original <- VCorpus(VectorSource(cleaned_data$cons))
 
-# maybe do function to abstract out and use on all three?
 
-corpus_transformer <- function(x) { # Making a function instead of writing this three times
-  tm_map(content_transformer(emoji_transformer))
-  tm_map(content_transformer(str_to_lower)) %>% # shift casing to lower
-    tm_map(removePunctuation) %>% # remove punctuation .....
-    tm_map(content_transformer(replace_contraction)) %>% # replace contractions with both words
-    tm_map(content_transformer(replace_abbreviation)) %>% # replace abbreviations with full words
-    tm_map(removeNumbers) %>% # remove numbers as they are irrelevant here
-    tm_map(content_transformer(lemmatize_words)) %>% # added content transformer to lemmatize becuase.......
-    tm_map(removeWords, stopwords("en")) %>% # Add more stopwords in c() as you look at the comparisons
-    tm_map(stripWhitespace)
-  
-  
-} # DO SOMETHING ABOUT BLANK CELLS BECUASE YOU WILL HAVE THEM
 
-headline_corpus <- corpus_transformer(headline_corpus_original)
 
-pros_corpus <- corpus_transformer(pros_corpus_original)
 
-cons_corpus <- corpus_transformer(cons_corpus_original)
+
+
+
+
 
 # use comparison code maybe delete maybe comment out afterwards
 
-compare_them <- function(x, y) { # make comarison function
-  i <- sample(seq_along(x), 1)
-  
-  cat("Corpus 1", " Row ", i, ": ", content(x[[i]]), "\n\n") # Used cat to make visually neat output for easy comparison
-  cat("Corpus 2", " Row ", i, ": ", content(y[[i]])) # Used cat to make visually neat output for easy comparison
-}
+# compare_them <- function(x, y) { # make comarison function
+#   i <- sample(seq_along(x), 1)
+#   
+#   cat("Corpus 1", " Row ", i, ": ", x[[i]], "\n\n") # Used cat to make visually neat output for easy comparison
+#   cat("Corpus 2", " Row ", i, ": ", y[[i]]) # Used cat to make visually neat output for easy comparison
+# } Seems to have done a decent job
+# 
+# 
+# compare_them(processed_data$review, processed_data$review_processed) # apply function
 
 
-compare_them(headline_corpus_original, headline_corpus) # apply function
-
-# DO EMBEDDINGS FOR EACH COLOUMN AND THEN ADDED TOPICS, AND EMBEDDINGS AS COLOUMNS to DATASET!!!!!!!!!!!!!!!!!!!
 
 # Analysis
